0baca65c905a22e5222b33f9b15aa906536657c4
Hyo Jung Lee <imhyojunglee@gmail.com>
2020-05-20 14:11:55 -0700

    init

--- src/main/resources/sechema.sql
+++ src/main/resources/sechema.sql
@@ -1,54 +1,54 @@
 CREATE TABLE oauth_access_token (
-    authentication_id varchar(256) COLLATE Korean_Wansung_CI_AS NOT NULL,
-    token_id varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
+    authentication_id varchar(256) NOT NULL,
+    token_id varchar(256) NULL,
     token varbinary NULL,
-    user_name varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    client_id varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
+    user_name varchar(256) NULL,
+    client_id varchar(256) NULL,
     authentication varbinary NULL,
-    refresh_token varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    CONSTRAINT PK__oauth_ac__7B2DE7F129FEC961 PRIMARY KEY (authentication_id)
+    refresh_token varchar(256) NULL,
+    PRIMARY KEY (authentication_id)
 );
 
 CREATE TABLE oauth_approvals (
-    userId varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    clientId varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    [scope] varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    status varchar(10) COLLATE Korean_Wansung_CI_AS NULL,
+    userId varchar(256) NULL,
+    clientId varchar(256) NULL,
+    [scope] varchar(256) NULL,
+    status varchar(10) NULL,
     expiresAt datetime NULL,
     lastModifiedAt datetime NULL
 );
 
 CREATE TABLE oauth_client_details (
-    client_id varchar(256) COLLATE Korean_Wansung_CI_AS NOT NULL,
-    resource_ids varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    client_secret varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    [scope] varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    authorized_grant_types varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    web_server_redirect_uri varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    authorities varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
+    client_id varchar(256) NOT NULL,
+    resource_ids varchar(256) NULL,
+    client_secret varchar(256) NULL,
+    [scope] varchar(256) NULL,
+    authorized_grant_types varchar(256) NULL,
+    web_server_redirect_uri varchar(256) NULL,
+    authorities varchar(256) NULL,
     access_token_validity int NULL,
     refresh_token_validity int NULL,
-    additional_information varchar(4096) COLLATE Korean_Wansung_CI_AS NULL,
-    autoapprove varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    CONSTRAINT PK__oauth_cl__BF21A4249B31C6A1 PRIMARY KEY (client_id)
+    additional_information varchar(4096) NULL,
+    autoapprove varchar(256) NULL,
+    PRIMARY KEY (client_id)
 );
 
 CREATE TABLE oauth_client_token (
-    token_id varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
+    token_id varchar(256) NULL,
     token varbinary NULL,
-    authentication_id varchar(256) COLLATE Korean_Wansung_CI_AS NOT NULL,
-    user_name varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    client_id varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
-    CONSTRAINT PK__oauth_cl__7B2DE7F166665A61 PRIMARY KEY (authentication_id)
+    authentication_id varchar(256) NOT NULL,
+    user_name varchar(256) NULL,
+    client_id varchar(256) NULL,
+    PRIMARY KEY (authentication_id)
 );
 
 CREATE TABLE oauth_code (
-    code varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
+    code varchar(256) NULL,
     authentication varbinary NULL
 );
 
 CREATE TABLE oauth_refresh_token (
-    token_id varchar(256) COLLATE Korean_Wansung_CI_AS NULL,
+    token_id varchar(256) NULL,
     token varbinary NULL,
     authentication varbinary NULL
 );
