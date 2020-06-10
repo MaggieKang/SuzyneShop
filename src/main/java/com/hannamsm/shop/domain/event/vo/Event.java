96ed0b1c3cfdae9fde3f9ca106b34d40f3525ca8
Hyo Jung Lee <imhyojunglee@gmail.com>
2020-06-10 09:04:55 -0700

    delete space

--- src/main/java/com/hannamsm/shop/domain/event/vo/Event.java
+++ src/main/java/com/hannamsm/shop/domain/event/vo/Event.java
@@ -29,14 +29,14 @@ public class Event extends BaseVO {
 	private boolean offline;
 	private boolean free;
 	private EventStatus eventStatus = EventStatus.DRAFT;
-	
+
 	public void update() {
 		if(this.basePrice == 0 && this.maxPrice == 0) {
 			this.free = true;
 		} else {
 			this.free = false;
 		}
-		
+
 		if(this.location == null || this.location.isBlank()) {
 			this.offline = false;
 		} else {
