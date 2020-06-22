d054f8bad789a14ab286e5a9d822671305d45d6f
Hyo Jung Lee <imhyojunglee@gmail.com>
2020-06-22 14:11:55 -0700

    add fileds for summery

--- src/main/java/com/hannamsm/shop/domain/cart/vo/CartSummery.java
+++ src/main/java/com/hannamsm/shop/domain/cart/vo/CartSummery.java
@@ -1,7 +1,6 @@
 package com.hannamsm.shop.domain.cart.vo;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,30 +13,11 @@ import lombok.ToString;
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @ToString
 public class CartSummery {
-
-	private String accountId;
-	private String itemId;
-	private String upc;
-	private String galCode;
-	private String prodOwnCode;
-	private String suppCode;
-	private String prodId;
-	private String itemKrNm;
-	private String itemEnNm;
-	private String itemSize;
-	private String itemType;
-	private String itemType2;
-	private BigDecimal itemInPrice;
-	private BigDecimal salePrice;
-	private BigDecimal regularPrice;
-	private int itemBalance;
-	private String taxCd;
-	private String saleUnit;
-	private String depositCd;
-	private String categoryCd;
-	private boolean isUse;
-	private LocalDateTime regDate;
-	private String regPerson;
-	private LocalDateTime lastModDate;
-	private String lastModPerson;
+	private int totalQty;
+	private BigDecimal totalSalesPrice;
+	private BigDecimal totalGstFee;
+	private BigDecimal totalPstFee;
+	private BigDecimal totalEcoFee;
+	private BigDecimal totalDepositFee;
+	private BigDecimal grandTotalPrice;
 }
