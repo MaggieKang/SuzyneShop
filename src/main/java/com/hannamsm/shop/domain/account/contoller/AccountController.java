715c86d4e1e2f48c2435a93e53eef9e66eb1c3e1
hj lee <imhyojunglee@gmail.com>
2020-07-16 10:14:28 -0700

    removed packge

--- src/main/java/com/hannamsm/shop/domain/account/contoller/AccountController.java
+++ src/main/java/com/hannamsm/shop/domain/account/contoller/AccountController.java
@@ -1,39 +1,21 @@
 package com.hannamsm.shop.domain.account.contoller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hannamsm.shop.domain.account.service.AccountService;
 import com.hannamsm.shop.domain.account.vo.Account;
-import com.hannamsm.shop.domain.account.vo.AuthenticationRequest;
-import com.hannamsm.shop.domain.account.vo.AuthenticationToken;
-import com.hannamsm.shop.domain.cart.vo.CartItem;
-import com.hannamsm.shop.domain.cart.vo.CartItemSearch;
-import com.hannamsm.shop.domain.profile.vo.Customer;
-import com.hannamsm.shop.global.adapter.CurrentUser;
 import com.hannamsm.shop.global.vo.ResponseResutl;
-import com.hannamsm.shop.global.vo.ResponseResutlsByPaging;
 
 @RestController
 @RequestMapping(value="/api/account", produces = MediaTypes.HAL_JSON_VALUE)
@@ -52,21 +34,21 @@ public class AccountController {
 		//accountEmail  password
 		System.out.println("queryCreateAccount username :"+reqAccount.getAccountEmail());
 		System.out.println("queryCreateAccount password :"+reqAccount.getPassword());
-		
+
 		//Account reqAccount = new Account();
 		//reqAccount.setAccountEmail(reqAccount.getAccountEmail());
 		//reqAccount.setPassword(reqAccount.getPassword());
-		
+
 		accountService.createUser(reqAccount);
 
 		//return data
 		ResponseResutl<Account> resResult = new ResponseResutl<Account>();
 		resResult.setMessage("회원가입이 완료 되었습니다.");
-		resResult.setResult(reqAccount);			
+		resResult.setResult(reqAccount);
 		return ResponseEntity.ok(resResult);
 	}
-	
-	
+
+
 //	@RequestMapping(value="/join", method=RequestMethod.POST)
 //	public AuthenticationToken join(@RequestBody AuthenticationRequest authenticationRequest, HttpSession session) {
 //		String userEmail = authenticationRequest.getUsername();
